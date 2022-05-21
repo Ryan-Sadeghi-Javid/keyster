fcb40df9198252adadc9b4ca80d744e09516d2f3
Ryan Sadeghi-Javid <58157787+HARRIBO81@users.noreply.github.com>
2022-05-21 19:36:11 -0400

    Update main.js

--- main.js
+++ main.js
@@ -6,10 +6,25 @@ const recognition = new window.SpeechRecognition();
 recognition.interimResults = true; // gives results as the user is talking
 
 let p = document.createElement('p');
+recognition.continuous = true; // avoids repeated confirmation of microphone usage in chrome
 
 recognition.addEventListener('result', (e) => {
     const text = Array.from(e.results).map(result=>result[0]).map(result=>result.transcript).join('');
-    console.log(text);
+    p.innerText = text;
+    texts.appendChild(p);
+    if(e.results[0].isFinal) {
+        if(text.includes('keister new tab')) {
+            p=document.createElement('p');
+            p.innerText='opening channel';
+            window.open("https://youtube.com");
+        }
+        p=document.createElement('p');
+    }
+    console.log(stringArray.slice(-5));
+})
+
+recognition.addEventListener('end', ()=>{
+    recognition.start();
 })
 
 recognition.start();
